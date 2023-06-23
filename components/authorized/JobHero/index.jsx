8ba8a3bdf23ab849a1522a9d235419d67a5417cd
Lilianada <lilibest30.lb@gmail.com>
2023-06-23 23:38:16 +0100

    job page hero

--- components/authorized/JobHero/index.jsx
+++ components/authorized/JobHero/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './style.scss';
+import Link from 'next/link';
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
 
 export default function JobHero() {
   return (
     <div className="jobHero__section">
         <div className="jobHero__content">
-            
+        <h1 className="hero__title">Find your dream job</h1>
+        <p className="hero__text">
+          We have over 100,000 jobs available for you
+        </p>
+        
+        <div className="hero__searchBar">
+          <input
+            type="search"
+            name="search-bar"
+            className="search__bar"
+            placeholder="Job Title, Skill or Company"
+          />
+          <Link href="/search" className="hero__button">
+            <HiOutlineArrowNarrowRight className="search__icon" />
+            <p>Find Jobs</p>
+          </Link>
+        </div>
         </div>
     </div>
   )
